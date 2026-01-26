16048b6019fe9a7907747d14d8ee21a023d956e2
friedchicken-wings <audrey.lin@ucas-edu.net>
2026-01-26 11:49:31 -0700

    financial

---
+++ Practices/financial_calculator.py
@@ -0,0 +1,7 @@
+#AL, Financial Calculator py
+monthly_income = float(input("how much money do you earn monthly?"))
+rent = float(input("what is your rent?"))
+utilities = float(input("what is your monthly utilities?"))
+groceries = float(input("what is your monthly groceries?"))
+transportation = float(input("what is your montly transportation?"))
+print("your rent is: $",rent, "and that is %",((rent/monthly_income)*100), "of your income.")
