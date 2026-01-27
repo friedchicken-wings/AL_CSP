53831dd6500f21915a801fbdc7fe9410c218159b
friedchicken-wings <audrey.lin@ucas-edu.net>
2026-01-27 11:47:07 -0700

    strings notes

---
+++ Notes/strings.py
@@ -0,0 +1,22 @@
+"""color = input("what is your favorite color: ").strip().title()
+
+print(color, "is a really cool color! ")"""""
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+sentence = "The quick brown fox jumps over the lazy dog." 
+alphabet.strip()
+
+"""print(alphabet[4:8])
+word = input("What word do you want to change: ") 
+new = input("What will the new word be: ")
+start = sentence.find("over")
+print("("+sentence[start:start+len(word)]+")")
+print(sentence)
+sentence = sentence.replace(word, new)
+print(sentence)"""
+
+number = int("Tell me a number: ")
+print(number/5)
+
+room = "Ms. Larose's room"
+quote = "Treyson said 'Yolo'"
