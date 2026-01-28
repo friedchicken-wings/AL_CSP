c2fd643b9854c15e942a734496184c79e2d10c11
friedchicken-wings <audrey.lin@ucas-edu.net>
2026-01-28 11:18:50 -0700

    silly sentences.py

---
+++ Notes/silly_sentences.py
@@ -0,0 +1,6 @@
+#AL, Silly Sentences
+place = input("Tell me a place: ").strip()
+verb = input("Tell me a verb: ")
+feeling = input("Tell me a feeling: ")
+verb2 = input("Tell me another verb: ")
+print("At the "+ place +" in the sun, we'll be "+ verb +" and having fun and it makes me feel so " + feeling + " I " + verb2)
