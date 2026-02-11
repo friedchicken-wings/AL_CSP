535b241da0da844f2e3de569dcd62756eecafa53
friedchicken-wings <audrey.lin@ucas-edu.net>
2026-02-11 11:49:21 -0700

    hangman

--- Practices/Hangman.py
+++ Practices/Hangman.py
@@ -2,25 +2,28 @@
 import random
 words = ["Chicken", "Maruchan", "Noodles", "Tacos", "Dumplings", "Pasta", "Hamburgers", "Steak", "Lasagna", "Fries"]
 word = random.choice(words)
+wrong = 0
 
-letters = ["A", "B", "C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+guess = input("Give me a letter.")
+if guess not in word:
+    wrong = + 1
+    print("Oops! You got it wrong. Try again")
 
+if guess in word:
+    print("Correct!")
 
-if letters in words:
-    print("letters")
 display = ""
-for display in letters:
+for letter in word:
+    if letter in word:
+        display + letter
+    else:
+        +"_": display
     print("Chicken")
-else:
-display2: Any("Marchan")
+    break
 
-
-import random 
-spell = C 
-words = random.randint["Chicken"]
-def wrong_guess_length(guess_word):
+"""def wrong_guess_length(guess_word):
     if(len(guess_word) < 5):
         print("Not enough CHaracters.")
     else:
         print("Too many characters.")
-    print("Try a guess with 5 letters.")
+    print("Try a guess with 5 letters.")"""
