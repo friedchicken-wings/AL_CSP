64c350145253dbdc314baf96834c542ac6027918
friedchicken-wings <audrey.lin@ucas-edu.net>
2026-02-27 11:36:42 -0700

    time of day

--- Practices/silly_sentences.c
+++ Practices/silly_sentences.c
@@ -1,22 +1,33 @@
 // AL, Silly Sentences
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    float noun;
-    float adjective;
-    float noun2;
+    char noun[50];
+    char adjective[50];
+    char noun2[50];
 
-    printf("\nTell me a noun.");
-    scanf("%f", &noun);
+    char sentence[300];
+    sentence[0] = '\0';
+
+    printf("\nTell me a place.");
+    scanf("%49s", &noun);
 
     printf("\nTell me an adjective.");
-    scanf("%f", &adjective);
+    scanf("%49s", &adjective);
 
-    printf("\nTell me another noun.");
-    scanf("%f", &noun2);
+    printf("\nGive me a name.");
+    scanf("%49s", &noun2);
 
-
-    strcat("\nAfter hiding the painting in his %f for two years, he grew %f and tried to sell it to a/an %f in Florence, but was caught.\n", noun, adjective, noun2);
-
+    strcat(sentence, "\nAfter hiding the painting in his ");
+    strcat(sentence, noun);
+    strcat(sentence, "\nfor two years, it grew ");
+    strcat(sentence, adjective);
+    strcat(sentence, "\nand tried to sell it to a/an ");
+    strcat(sentence, noun2);
+    strcat(sentence, "\nin Florence, but was caught.");
+    
+    printf("\n%s\n", sentence);
+    
     return 0;
 }
