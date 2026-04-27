fa92dfa860c75e8e22e565a20071290b8cb97205
friedchicken-wings <audrey.lin@ucas-edu.net>
2026-04-27 11:48:24 -0600

    staycation

--- webdev/notes/FLASK/app.py
+++ webdev/notes/FLASK/app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hello World</p>"
+    return render_template("name.html", title="Home Page", content="This is the home page of my website")
 
 @app.route("/<name>")
 def user(name):
-    return f"Hello {name}!"
+    return  render_template("name.html", title=f"{name}'s Page", content=f"This is the individualized content for {name}")
 
 @app.route("/test")
 def testing():
-    return "<p>This is a test</p>"
+    return render_template("name.html", title="Test Page", content="This is just a test to see if things work!")
 
 """@app.route('/login', methods=['GET', 'POST'])
 def login():
